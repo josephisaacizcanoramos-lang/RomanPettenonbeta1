f7f5af170decb4045b7c681990b41c11f9dba89a
josephisaacizcanoramos-lang <josephisaacizcanoramos@gmail.com>
2026-06-08 22:18:37 -0600

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,8 @@
 import { hydrateRoot } from "react-dom/client";
 import { StartClient } from "@tanstack/react-start";
 import { getRouter } from "./router";
+import { createElement } from "react";
 
 const router = getRouter();
 
-hydrateRoot(document, <StartClient router={router} />);
+hydrateRoot(document, createElement(StartClient, { router }));
